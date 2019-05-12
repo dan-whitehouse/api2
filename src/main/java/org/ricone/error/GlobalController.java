379406ff1dc25593baf4e7f153a2284b8c022a58
Dan <Dan@DESKTOP>
2019-05-11 22:30:58 -0400

    v2.0.0 - Global Controller, 404

--- src/main/java/org/ricone/error/GlobalController.java
+++ src/main/java/org/ricone/error/GlobalController.java
@@ -1,12 +1,17 @@
 package org.ricone.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 public class GlobalController {
 
+	/*@RequestMapping(value="/**", method = RequestMethod.GET)
+	public String getAnythingelse() {
+		return "redirect:/error.html";
+	}*/
 }
